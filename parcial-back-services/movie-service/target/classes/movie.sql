7dbfd2ffcf7ad5dce45d6db18d06e2d7851e548f
agusreina18 <agusreina18.ar@gmail.com>
2022-08-29 23:03:01 -0300

    ADD Parcial Files

---
+++ parcial-back-services/movie-service/target/classes/movie.sql
@@ -0,0 +1,8 @@
+INSERT INTO movies(name, genre, url_stream) VALUES('Love Story', 'Romantic', 'https://www.netflix.com/title/70217909');
+INSERT INTO movies(name, genre, url_stream) VALUES('Titanic', 'Romantic', 'https://www.netflix.com/title/562050');
+INSERT INTO movies(name, genre, url_stream) VALUES('Minions', 'Comedy', 'https://www.netflix.com/title/80033394');
+INSERT INTO movies(name, genre, url_stream) VALUES('Doblemente Embarazada', 'Comedy', 'https://www.netflix.com/title/70262639');
+INSERT INTO movies(name, genre, url_stream) VALUES('Love, Rosie', 'Romantic', 'https://www.netflix.com/title/70075480');
+INSERT INTO movies(name, genre, url_stream) VALUES('Esperando la Carroza', 'Comedy', 'https://www.netflix.com/title/70267241');
+INSERT INTO movies(name, genre, url_stream) VALUES('El exorcista', 'Terror', 'https://www.netflix.com/title/81038963');
+INSERT INTO movies(name, genre, url_stream) VALUES('Rocky IV', 'Accion', 'https://www.netflix.com/title/70032594');
